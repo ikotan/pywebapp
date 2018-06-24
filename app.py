6a9e09d9660fc061721abdbc5954591900704d70
ikotan <ikotan.69@gmail.com>
2018-06-24 01:48:08 +0000

    推定年齢ロジックの実装

--- app.py
+++ app.py
@@ -1,20 +1,65 @@
 from bottle import debug
 debug(True)
+from bottle import (default_app, route, run, template, request, HTTPError)
+from abalone_predictor import AbalonePredictor
+from collections import namedtuple
+from bokeh.plotting import figure
+from bokeh.embed import components
 
-from bottle import route, run, template
+READABLE_SEX = ['メス', '不明', 'オス']
+
+INPUT_DATA = ('sex', 'length', 'diameter', 'height', 'weight')
+
+BaseAbalone = namedtuple('BaseAbalone', INPUT_DATA + ('age', ))
+
+
+class Abalone(BaseAbalone):
+    @property
+    def sex_str(self):
+        return READABLE_SEX[int(self.sex)]
+
 
 @route('/')
 def index():
     return template('templates/index.tpl')
 
+
 @route('/abalone', method='POST')
 def result():
-    return template('templates/result.tpl',
-                    sex='不明',
-                    length=0,
-                    diameter=0,
-                    height=0,
-                    weight=0,
-                    age=0)
+    try:
+        age = calc_age(**request.params)
+        abalone = Abalone(age=age, **request.params)
+    except (KeyError, ValueError) as e:
+        raise HTTPError(status=400, body=e)
+    script, div = get_graph(abalone)
+    return template(
+        'templates/result.tpl', abalone=abalone, script=script, graph=div)
 
-run(host='192.168.40.10', port=8888, reloader=True)
+
+_predictor = AbalonePredictor()
+
+
+def calc_age(sex, length, diameter, height, weight):
+    age = _predictor.predict(
+        int(sex), int(length), int(diameter), int(height), int(weight))
+    return float(age)
+
+
+def get_graph(abalone):
+    p = figure(plot_width=400, plot_height=400, title='実年齢と推定値の分布')
+    p.xaxis.axis_label = '実年齢'
+    p.yaxis.axis_label = '推定値'
+    p.line([0, 30], [0, 30], line_dash='dotted', legend='実年齢と推定値が一致するライン')
+    p.circle(_predictor.y_train, _predictor.prediction, legend='訓練データにおける分布')
+    p.line(
+        [0, 30], [abalone.age, abalone.age],
+        legend='捕まえたアワビの推定年齢',
+        color="green")
+    p.legend.location = 'top_left'
+    p.legend.click_policy = 'mute'
+    script, div = components(p)
+    return script, div
+
+application = default_app()
+
+#  run(host='192.168.40.10', port=8888, reloader=True)
--- templates/base.tpl
+++ templates/base.tpl
@@ -4,6 +4,12 @@
     <meta charset="utf-8">
     <title>アワビ！</title>
     <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v2.3.7/dist/mini-default.min.css">
+    % setdefault('use_bokeh', False)
+    % if use_bokeh:
+      <link href="http://cdn.pydata.org/bokeh/release/bokeh-0.12.13.min.css" rel="stylesheet" type="text/css">
+      <script src="http://cdn.pydata.org/bokeh/release/bokeh-0.12.13.min.js"></script>
+      {{ !script }}
+    % end
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
